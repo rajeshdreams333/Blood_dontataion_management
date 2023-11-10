1573d50b8d239c3d2f698116b6ae485dbc1f0ba0
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-11-10 23:02:41 +0530

    Post and get calls has build

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,63 @@
-import  express  from "express";
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
 
-import  mysql, {createConnection }from "mysql";
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '8096',
+  database: 'BDMS',
+  insecureAuth : true
+});
 
-const app=express()
+app.use(express.json())
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+app.get("/donar",(req,res)=>{
+    const q= "select * from  donar"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.get("/patient",(req,res)=>{
+    const q= "select * from patient"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.post("/donar",(req,res)=>{
+    const q="INSERT INTO donar (`Donar_Name`,`Blood_Group`,`Contact_Number`) values(?)"
+    const values=[
+        req.body.Donar_Name,
+        req.body.Blood_Group,
+        req.body.Contact_Number
+    ];
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Donar data added Successfully....");
+    })
+})
+app.post("/patient",(req,res)=>{
+    const q="INSERT INTO patient (`Name`,`Purpose`,`Contact_Number`) values(?)"
+    const values=[
+        req.body.Name,
+        req.body.Purpose,
+        req.body.Contact_Number
+    ];
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Patient data added Successfully....");
+    })
+})
 
-const db=mysql.createConnection({
-    host:"RAJESHDREAMS333",
-    username:"root",
-    password:"8096",
-    database:"test"
-})
-app.get('/',(req,res)=>{
-    res.json("Hello this is backend......")
-})
-app.listen(8800,()=>{
-    console.log("Connect to Backend....!");
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
