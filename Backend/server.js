a70aa9c7054134d084699a70d864b364cf30e69c
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-12-06 19:54:49 +0530

    deploy-1

--- Backend/server.js
+++ Backend/server.js
@@ -3,121 +3,121 @@ const mysql = require("mysql2");
 const app = express();
 const cors = require("cors");
 
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "8096",
-//   database: "BDMS",
-//   insecureAuth: true,
-// });
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "8096",
+  database: "BDMS",
+  insecureAuth: true,
+});
 app.get("/", (req, res) => {
   res.json("Hello....");
 });
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["https://beedatabdms.web.app"],
-//     methods: ["POST", "GET", "DELETE"],
-//     credentials: true,
-//   })
-// );
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Database connection failed: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to the database");
-// });
-// app.get("/donar", (req, res) => {
-//   const q = "select * from  donar";
-//   db.query(q, (err, data) => {
-//     if (err) return res.json(err);
-//     return res.json(data);
-//   });
-// });
-// app.get("/blood", (req, res) => {
-//   const { Blood_group, Units } = req.body;
-//   const q = "select * from  blood";
-//   db.query(q, (err, data) => {
-//     if (err) return res.json(err);
-//     return res.json(data);
-//   });
-// });
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://beedatabdms.web.app"],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to the database");
+});
+app.get("/donar", (req, res) => {
+  const q = "select * from  donar";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+app.get("/blood", (req, res) => {
+  const { Blood_group, Units } = req.body;
+  const q = "select * from  blood";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-// app.post("/donar", (req, res) => {
-//   const { Name, Gender, Age, Mobile, Email, Blood_group, Address } = req.body;
-//   const q = `INSERT INTO donar(Name,Gender,Age,Mobile,Email,Blood_group,Address) values(?)`;
-//   db.query(
-//     q,
-//     [[Name, Gender, Age, Mobile, Email, Blood_group, Address]],
-//     (err, result) => {
-//       if (err) {
-//         console.error("Database error: " + err);
-//         res.status(500).json({ success: false, message: "Donar Added Failed" });
-//       } else {
-//         res.json({ success: true, message: "Donar Added Successfully" });
-//       }
-//     }
-//   );
-// });
-// app.post("/blood", (req, res) => {
-//   const { Blood_group, Units } = req.body;
-//   const q = `update blood set Units = Units + ? where Blood_group = ?`;
-//   db.query(q, [Number(Units), Blood_group], (err, result) => {
-//     if (err) {
-//       console.error("Database error: " + err);
-//       res.status(500).json({ success: false, message: "Blood Added Failed" });
-//     } else {
-//       res.json({ success: true, message: "Blood Added Successfully" });
-//     }
-//   });
-// });
-// app.delete("/remove/:iddonar", (req, res) => {
-//   const { iddonar } = req.params;
-//   const q = "DELETE FROM donar WHERE iddonar = ?;";
-//   db.query(q, [iddonar], (err, result) => {
-//     if (err) {
-//       console.log("Database Error:" + err);
-//       res
-//         .status(500)
-//         .json({ success: false, message: "Donar Deleted Failed.." });
-//     } else {
-//       res.json({ success: true, message: "Donar Deleted Successfully.." });
-//     }
-//   });
-// });
-// app.post("/patients", (req, res) => {
-//   const { Name, Units, Blood_group, Purpose } = req.body;
-//   const q1 = `INSERT INTO patient(Name, Units, Blood_group, Purpose) VALUES (?)`;
-//   const insertPatientPromise = new Promise((resolve, reject) => {
-//     db.query(q1, [[Name, Units, Blood_group, Purpose]], (err, result) => {
-//       if (err) {
-//         console.error("Database error (insert patient): " + err);
-//         reject("Patient Added Failed");
-//       } else {
-//         resolve("Patient Added Successfully");
-//       }
-//     });
-//   });
-//   const q2 = `UPDATE blood SET Units = Units - ? WHERE Blood_group = ?`;
-//   const updateBloodPromise = new Promise((resolve, reject) => {
-//     db.query(q2, [Number(Units), Blood_group], (err, result) => {
-//       if (err) {
-//         console.error("Database error (update blood): " + err);
-//         reject("Blood Added Failed");
-//       } else {
-//         resolve("Blood Added Successfully");
-//       }
-//     });
-//   });
-//   Promise.all([insertPatientPromise, updateBloodPromise])
-//     .then((messages) => {
-//       res.json({ success: true, messages });
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ success: false, message: error });
-//     });
-// });
+app.post("/donar", (req, res) => {
+  const { Name, Gender, Age, Mobile, Email, Blood_group, Address } = req.body;
+  const q = `INSERT INTO donar(Name,Gender,Age,Mobile,Email,Blood_group,Address) values(?)`;
+  db.query(
+    q,
+    [[Name, Gender, Age, Mobile, Email, Blood_group, Address]],
+    (err, result) => {
+      if (err) {
+        console.error("Database error: " + err);
+        res.status(500).json({ success: false, message: "Donar Added Failed" });
+      } else {
+        res.json({ success: true, message: "Donar Added Successfully" });
+      }
+    }
+  );
+});
+app.post("/blood", (req, res) => {
+  const { Blood_group, Units } = req.body;
+  const q = `update blood set Units = Units + ? where Blood_group = ?`;
+  db.query(q, [Number(Units), Blood_group], (err, result) => {
+    if (err) {
+      console.error("Database error: " + err);
+      res.status(500).json({ success: false, message: "Blood Added Failed" });
+    } else {
+      res.json({ success: true, message: "Blood Added Successfully" });
+    }
+  });
+});
+app.delete("/remove/:iddonar", (req, res) => {
+  const { iddonar } = req.params;
+  const q = "DELETE FROM donar WHERE iddonar = ?;";
+  db.query(q, [iddonar], (err, result) => {
+    if (err) {
+      console.log("Database Error:" + err);
+      res
+        .status(500)
+        .json({ success: false, message: "Donar Deleted Failed.." });
+    } else {
+      res.json({ success: true, message: "Donar Deleted Successfully.." });
+    }
+  });
+});
+app.post("/patients", (req, res) => {
+  const { Name, Units, Blood_group, Purpose } = req.body;
+  const q1 = `INSERT INTO patient(Name, Units, Blood_group, Purpose) VALUES (?)`;
+  const insertPatientPromise = new Promise((resolve, reject) => {
+    db.query(q1, [[Name, Units, Blood_group, Purpose]], (err, result) => {
+      if (err) {
+        console.error("Database error (insert patient): " + err);
+        reject("Patient Added Failed");
+      } else {
+        resolve("Patient Added Successfully");
+      }
+    });
+  });
+  const q2 = `UPDATE blood SET Units = Units - ? WHERE Blood_group = ?`;
+  const updateBloodPromise = new Promise((resolve, reject) => {
+    db.query(q2, [Number(Units), Blood_group], (err, result) => {
+      if (err) {
+        console.error("Database error (update blood): " + err);
+        reject("Blood Added Failed");
+      } else {
+        resolve("Blood Added Successfully");
+      }
+    });
+  });
+  Promise.all([insertPatientPromise, updateBloodPromise])
+    .then((messages) => {
+      res.json({ success: true, messages });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, message: error });
+    });
+});
 
 // const port = process.env.PORT || 5000;
 app.listen(5000, () => {
